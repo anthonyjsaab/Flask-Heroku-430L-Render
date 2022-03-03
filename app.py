c5462656ceebcf959ce5ef2f837d9c17093d43ee
Anthony <56998706+ajs34@users.noreply.github.com>
2022-03-03 06:52:56 +0200

    test #3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 app = Flask(__name__)
 app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost:3306/exchange'
+    'SQLALCHEMY_DATABASE_URI'] = 'postgres://wkjvumxnirhcbx:046c40445f3e128229090cb1b25f84769559e8dd215a1ac9b13137af47812ea9@ec2-34-206-148-196.compute-1.amazonaws.com:5432/dfsi1fcrvq2qaa'
 CORS(app)
 db = SQLAlchemy(app)
 
