312b975403716bbf0e150e6ea6cba87196b5240b
Anthony J. Saab <ajs34@mail.aub.edu>
2022-03-25 15:01:10 +0200

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,1 +1,1 @@
-DB_CONFIG = 'postgresql://wkjvumxnirhcbx:046c40445f3e128229090cb1b25f84769559e8dd215a1ac9b13137af47812ea9@ec2-34-206-148-196.compute-1.amazonaws.com:5432/dfsi1fcrvq2qaa'
+DB_CONFIG = 'postgresql://namdvolrdjhlpn:78baa59a22afa567e852dae1aecab840d3a95dfc19ffe1fac9534071c7938738@ec2-52-3-60-53.compute-1.amazonaws.com:5432/dd24amg82jv611'
